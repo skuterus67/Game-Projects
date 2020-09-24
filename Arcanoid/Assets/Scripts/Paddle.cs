14042923ee4ae559afc8fef4852b4aa1257692cf
skuterus67 <milosstan@autograf.pl>
2020-09-25 00:43:19 +0200

    Initial commit First level setup Second level setup Audio added

---
+++ Arcanoid/Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float unitSize = 16f;
+    [SerializeField] float min, max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * unitSize;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, min, max);
+        transform.position = paddlePos;
+    }
+}
