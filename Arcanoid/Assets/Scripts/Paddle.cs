19542016b8317ce918c4cdeb37b155b602ef19dc
skuterus67 <milosstan@autograf.pl>
2020-09-28 13:41:55 +0200

    Added autoplay finished the prototype

--- Arcanoid/Assets/Scripts/Paddle.cs
+++ Arcanoid/Assets/Scripts/Paddle.cs
@@ -7,18 +7,33 @@ public class Paddle : MonoBehaviour
     [SerializeField] float unitSize = 16f;
     [SerializeField] float min, max;
 
+    GameSession gameSession;
+    Ball ball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameSession = FindObjectOfType<GameSession>();
+        ball = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * unitSize;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, min, max);
+        paddlePos.x = Mathf.Clamp(GetXPos(), min, max);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (gameSession.IsAutoPlayEnabled())
+        {
+            return ball.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * unitSize;
+        }
     }
 }
