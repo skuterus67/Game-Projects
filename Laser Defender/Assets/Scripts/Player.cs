c9c7ab99986fcf27f6775d025ec9e656e7d78e4a
skuterus67 <milosstan@autograf.pl>
2020-09-29 00:29:14 +0200

    Initial Commit Assets Player movement

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position = new Vector2(CalculateXPos(), CalculateYPos());
+    }
+
+    private float CalculateXPos()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        return newXPos;
+    }
+
+    private float CalculateYPos()
+    {
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        return newYPos;
+    }
+}
