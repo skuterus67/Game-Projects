51ca691d29b4000f1cad84c2f6e8a8054460a28a
skuterus67 <milosstan@autograf.pl>
2020-10-05 23:24:12 +0200

    Initial Commit Scene Setup

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int sceneToLoadIndex;
+    [SerializeField] int waitSeconds = 3;
+
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            LoadStartScreen();
+        }
+    }
+
+    public void LoadStartScreen()
+    {
+        StartCoroutine(WaitAndLoad(sceneToLoadIndex, waitSeconds));
+    }
+
+    IEnumerator WaitAndLoad(int sceneIndex, int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        LoadNextScene(sceneIndex);
+    }
+
+    public void LoadNextScene(int currentIndex)
+    {
+        SceneManager.LoadScene(currentIndex);
+    }
+
+}
